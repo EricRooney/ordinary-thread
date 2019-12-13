7e5198bddd3a02705238dfae2d82702ad385bb51
EricRooney <erooneous@gmail.com>
2019-12-13 10:26:13 -0700

    UserService Done

---
+++ server/services/PostsService.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import Post from "../models/Post";
+import ApiError from "../utils/ApiError";
+
+const _repository = mongoose.model("Post", Post);
+
+class PostService {
+  /*async getAll() {
+    return await _repository.find({});
+  }*/
+  async getById(id) {
+    let data = await _repository.findOne({ _id: id });
+    if (!data) {
+      throw new ApiError("Invalid Id", 400);
+    }
+    return data;
+  }
+  async getByName(searchName) {
+    let data = await _repository.findOne({ name: searchName });
+    if (!data) {
+      throw new ApiError("Invalid Name", 400);
+    }
+    return data;
+  }
+  async create(rawData) {
+    return await _repository.create(rawData);
+  }
+  async edit(id, update) {
+    let data = await _repository.findOneAndUpdate({ _id: id }, update, {
+      new: true
+    });
+    if (!data) {
+      throw new ApiError("Invalid Id", 400);
+    }
+    return data;
+  }
+  async delete(id) {
+    let data = await _repository.findOneAndDelete({ _id: id });
+    if (!data) {
+      throw new ApiError("Invalid Id", 400);
+    }
+  }
+}
+
+const postService = new PostService();
+export default postService;
